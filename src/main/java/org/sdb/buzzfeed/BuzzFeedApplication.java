ad435bdad0f8041a81a01d68adef296ba6360ac9
cwanzhen <2016129587@qq.com>
2025-09-19 15:02:48 +0800

    Initial commit

---
+++ src/main/java/org/sdb/buzzfeed/BuzzFeedApplication.java
@@ -0,0 +1,13 @@
+package org.sdb.buzzfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuzzFeedApplication.class, args);
+    }
+
+}
